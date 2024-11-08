615c02703d23e8139d2ed1ae199d60dbc4a1abb2
Daniel Jacobowitz <djacobowitz@web.de>
2024-11-08 22:44:32 +0100

    Ergänzungen zum Code

--- bubu.py
+++ bubu.py
@@ -3,3 +3,7 @@ answer = input("Wer ist deine große Liebe?")
 if answer == "Bebi" or answer == "Daniel":
     print("Das ist korrekt!")
 else: print("Das solltest du dir nochmal überlegen")
+
+print("Ich bin der Beste!")
+
+print("Hallo")
